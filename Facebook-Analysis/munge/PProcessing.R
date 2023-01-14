11eab4325ad2bc8123fd2815ab68e072c50b2a7f
YashBorikar <ysborikar24@gmail.com>
2023-01-14 19:09:16 +0000

    preprocessing file

---
+++ Facebook-Analysis/munge/PProcessing.R
@@ -0,0 +1,35 @@
+# Columns names
+col_name <- c("Timestamp","Packet_Length","Source_IP","Destination_IP",
+          "Source_L4_Port", "Destination L4 Port", "IP_protocol", 
+          "Source_Hostprefix","Destination_hostprefix",
+          "Source_Rack", "Destination_Rack",
+          "Source_Pod", "Destination_Pod",
+          "Intercluster", "Interdatacenter")
+ 
+ # Combining all data-file of cluster A
+ cluster_a_data = bind_rows(X10000000_183453745395047_5277553940995506176_n,
+                           X10000000_285913731807310_7357025347344269312_n,
+                           X10000000_321929591496191_6386372367764946944_n,
+                           X10000000_342423096094003_8905061200115531776_n,
+                           X10000000_538907286298295_2100867541848031232_n
+                            )
+ # Assign column names
+ colnames(cluster_a_data) <- col_name
+ # Convert Time stamp
+ cluster_a_data$Timestamp <- as.POSIXct(cluster_a_data$Timestamp, origin = "1970-01-01")
+ #Removing NA values from data set
+ cluster_a_data <- subset(cluster_a_data, !grepl('\\N', Destination_Pod))
+ 
+ # Combining all data-file of cluster C
+ cluster_c_data = bind_rows(X10000000_297743010611337_296246898249957376_n,
+                            X10000000_313485885684915_2821160804659757056_n,
+                            X10000000_390290874635322_4151848458732437504_n,
+                            X10000000_1082294968505483_1087203793398923264_n,
+                            X10000000_1475041669179819_5579678115442982912_n
+                            )[1:15]
+ # Assign column names
+ colnames(cluster_c_data) <- col_name
+ # Convert Time stamp
+ cluster_c_data$Timestamp <- as.POSIXct(cluster_c_data$Timestamp, origin = "1970-01-01")
+ #Removing NA values from data set
+ cluster_c_data <- subset(cluster_c_data, !grepl('\\N', Destination_Pod))
