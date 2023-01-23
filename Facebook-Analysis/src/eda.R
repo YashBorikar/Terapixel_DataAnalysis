efdd403f7202df63c017577796096dfe5a63def8
YashBorikar <ysborikar24@gmail.com>
2023-01-23 22:45:13 +0000

    Changes in preprocessing script and script.py file

--- Facebook-Analysis/src/eda.R
+++ Facebook-Analysis/src/eda.R
@@ -1,8 +1,21 @@
 library('ProjectTemplate')
 load.project()
 
-for (dataset in project.info$data)
-{
-  message(paste('Showing top 5 rows of', dataset))
-  print(head(get(dataset)))
-}
+
+# two_mins = cluster_a_data[grep('[0|5]$', format(cluster_a_data$Time, '%M')),]distinct(df1)
+print(length(distinct(cluster_a_data['Source_IP'])))
+n_distinct(cluster_a_data$Source_IP)
+n_distinct(cluster_a_data$Destination_IP)
+
+
+# Source destination pair with packet size in cluster A
+src_des_pck_size_a <- as.POSIXct(min(cluster_a_data$Timestamp))
+src_des_pck_size_a
+time_min <- as.POSIXct(src_des_pck_size_a, format = "%Y-%m-%d %H:%M:%S BST")
+typeof(time_min)
+add_time <- time_min + (5*60)
+data <- cluster_a_data[cluster_a_data$Timestamp >= time_min & cluster_a_data$Timestamp <= add_time,]
+data
+
+# Source destination pair with packet size in cluster C
+src_des_pck_size_b <- 
